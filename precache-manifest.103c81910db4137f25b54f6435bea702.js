6cd441b11c1f56156a4e2894af0509fdef4f5451
Robert <rsobieszek89@gmail.com>
2019-06-19 09:52:56 +0200

    Updates

---
+++ precache-manifest.103c81910db4137f25b54f6435bea702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89d962ec80343eb1bcbfff2615fd9a6",
+    "url": "/Baby-sleep-pattern-app/index.html"
+  },
+  {
+    "revision": "c774c54b62b23aa15f70",
+    "url": "/Baby-sleep-pattern-app/static/css/main.3baddd3b.chunk.css"
+  },
+  {
+    "revision": "afa8dd19194287504c5b",
+    "url": "/Baby-sleep-pattern-app/static/js/2.a32b54f4.chunk.js"
+  },
+  {
+    "revision": "c774c54b62b23aa15f70",
+    "url": "/Baby-sleep-pattern-app/static/js/main.72e2baad.chunk.js"
+  },
+  {
+    "revision": "0bba3344641af32242ff",
+    "url": "/Baby-sleep-pattern-app/static/js/runtime~main.9050275b.js"
+  }
+]);
